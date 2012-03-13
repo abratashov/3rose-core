389dddd1bed636906c9adfa844ad8b44fc6bf0a5
Bratashov Aleksandr <abratashov@gmail.com>
2012-03-13 21:43:35 +0200

    create core

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoseCore::Application.config.secret_token = '8762a7efd51d9a4e4659dd6ad5f6cc330a94e232771a393031d907e81214ba146387aa611eb4b36b14abab940ef349ac83f836f2963fdc44f011e4111d808459'
